498e396f6822b0cf33cb4c968534d22fa9bb4dd7
micaelalay <micalay003@gmail.com>
2025-07-14 20:53:54 -0300

    added go to previous screen to back button

--- app/medication/add.tsx
+++ app/medication/add.tsx
@@ -243,7 +243,7 @@ export default function AddMedicationScreen() {
       />
       <View style={styles.content}>
         <View style={styles.header}>
-          <TouchableOpacity style={styles.backButton}>
+          <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
             <Ionicons name="chevron-back" size={28} color={"#1A8E2D"} />
           </TouchableOpacity>
           <Text style={styles.headerTitle}>New Medication</Text>
